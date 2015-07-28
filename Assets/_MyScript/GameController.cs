977ed5a7c1b7317ef3a2c0f617956346899772b4
jayden-kim <biglobster03@gmail.com>
2015-07-29 02:27:36 +0900

    (*) 몬스터 랜덤 위치에 증식....  - GameController.cs 추가 (몬스터 생성관리)  - MonsterStarter.cs 추가 (몬스터 이동관련.. 이름 바꿔야겠네)
     - ( random(-8, 7), -3) 의 위치에 생성

---
+++ Assets/_MyScript/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject moster;
+	public float startWait = 5;
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine (SpawnWaves ());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	IEnumerator SpawnWaves()
+	{
+		//yield return new WaitForSeconds (1);
+
+		while (true)
+		{
+			GameObject hazard = moster;
+			Vector3 spawnPosition = new Vector3 (Random.Range (-8, 7), -3, 1);
+			Instantiate (hazard, spawnPosition, Quaternion.identity);
+
+			yield return new WaitForSeconds (startWait);
+		}
+	}
+}
---
+++ Assets/_MyScript/MonsterStarter.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterMover : MonoBehaviour
+{
+	public float speed = 1.0f;
+	public enum MonsterMoveState
+	{
+		TURNUP,
+		MOVE
+	};
+	private MonsterMoveState currentState = MonsterMoveState.TURNUP;
+	// Use this for initialization
+
+	void Start ()
+	{
+		GetComponent<Rigidbody> ().velocity = new Vector2 (0, 2) * speed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void FixedUpdate ()
+	{
+		// check monster move state
+		float dirX = Random.Range (-5, 5);
+		Vector2 movement = new Vector2 (dirX, 0);	
+
+		GetComponent<Rigidbody2D>().velocity = movement * speed;
+
+		// move up
+		if (currentState == MonsterMoveState.TURNUP)
+			MoveUp ();
+		else if (currentState == MonsterMoveState.MOVE)
+			MoveHorizontalRandom ();
+	}
+
+	void MoveUp()
+	{
+	}
+
+	void MoveHorizontalRandom ()
+	{
+	}
+}
